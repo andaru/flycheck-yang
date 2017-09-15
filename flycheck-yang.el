01f06d689849380634bf036d832dc655662cf52d
Andrew Fort <afort@arista.com>
2017-09-15 13:08:40 -0700

    Add GPL license

--- flycheck-yang.el
+++ flycheck-yang.el
@@ -5,6 +5,21 @@
 ;; Version: 0.0
 ;; Package-Requires: ((yang-mode "0.9.4") (flycheck "0.18"))
 
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This package configures provides YANG syntax checking via flycheck
 ;; in Emacs using the pyang YANG parser[1].
